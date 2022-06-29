1c773ddf3c58a973809708cf0f5b0369958d87bf
Josue Salazar <salazarjosue2604@gmail.com>
2022-06-29 17:33:36 -0500

    Ver. 02

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import portfolio
+from .views import portfolio, project_details   
 
 app_name = 'portfolio'
 
 urlpatterns = [
     path('portfolio/', portfolio, name='portfolio'),
+    path('portfolio/<int:project_id>', project_details, name='project_details'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 from .models import Project
 
 # Create your views here.
@@ -6,3 +7,11 @@ def portfolio(request):
     projects = Project.objects.all()
 
     return render(request, 'projects.html', {'projects' : projects})
+
+def project_details(request, project_id):
+    try:
+        project = Project.objects.get(pk=project_id)
+        return render(request, 'project_details.html', {'project' : project})
+        
+    except Project.DoesNotExist:
+        raise Http404("No MyModel matches the given query. Error: 404")
